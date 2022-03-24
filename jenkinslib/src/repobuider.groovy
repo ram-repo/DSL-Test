984a97b45e0ce8b9ebc47151756e36faa2d2d15d
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-24 23:33:51 +0530

    Create repobuider.groovy

---
+++ jenkinslib/src/repobuider.groovy
@@ -0,0 +1,46 @@
+def createNewJenkinsJobWithMultiBranch(String projectsFolder, String projectName, String destProject, String destGit, String githubid) {
+    // Get GIT Creds and URL, need to replace everything from /scm/.* and https
+    scmCredsID = scm.getUserRemoteConfigs()[0].getCredentialsId()
+    scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
+    gitServerHost = scmUrl.replaceAll("http://", "").replaceAll("https://", "").replaceAll("/scm/.*", "")
+    StandardUsernamePasswordCredentials creds = CredentialsProvider.findCredentialById("${scmCredsID}",
+            StandardUsernamePasswordCredentials.class, currentBuild.rawBuild)
+    def gitUser = creds.getUsername()
+
+    jobDsl additionalParameters: [
+        projectsFolder: projectsFolder,
+        projectName: projectName,g
+        destProject: destProject,
+        destGit: destGit,
+        gitUserUri: gitUser.replace("@", "%40"),
+        gitServerHost: gitServerHost,
+        scmCredsID: scmCredsID
+    ], scriptText: '''
+    multibranchPipelineJob("${projectsFolder}/${destProject}") {
+    branchSources {
+        github {
+            id("${githubid}") // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId("${scmCredsID}") // GITHUB_ACCESS
+            repoOwner("${projectName}")
+            repository("${destProject}")
+            includes("master feature/* bugfix/* hotfix/* release/*")
+            excludes("donotbuild/*")
+        }
+    }
+  	factory {
+        workflowBranchProjectFactory {
+            scriptPath("jenkinsFile.groovy")
+        }
+    }
+    triggers {
+        periodicFolderTrigger {
+            interval("2m")
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+        }
+    }
+}'''
+}
